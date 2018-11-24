479a5d9b8316c74981b67bb98d230a5a8bcf1983
Ron Pinz <ron.pinz@gmail.com>
2018-11-23 19:49:11 -0500

    add manifest for swift 5

---
+++ Package@swift-5.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Gdk",
+    products: [
+        .library(
+            name: "Gdk",
+            targets: ["Gdk"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/rpinz/SwiftGdkPixbuf",
+            .branch("master")),
+        .package(
+            url: "https://github.com/rpinz/SwiftPangoCairo",
+            .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "Gdk",
+            dependencies: ["CGdk","GdkPixbuf", "PangoCairo"],
+            path: "Sources"),
+        .testTarget(
+            name: "GdkTests",
+            dependencies: ["Gdk"]),
+        .systemLibrary(
+            name: "CGdk",
+            path: "Library/CGdk",
+            pkgConfig: "gdk-3.0",
+            providers: [
+                .brew(["libgtk-3"]),
+                .apt(["libgtk-3-dev"])
+            ])
+    ],
+    swiftLanguageVersions: [
+        .v4_2
+    ]
+)
